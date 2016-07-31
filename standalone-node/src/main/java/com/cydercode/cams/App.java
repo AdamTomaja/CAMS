2bc4bd25f17738ebbb684f5e367f4e0b6c7ebd81
AdamTomaja <adiscool@tlen.pl>
2016-07-31 16:13:38 +0200

    standalone-node project and modules structure

---
+++ standalone-node/src/main/java/com/cydercode/cams/App.java
@@ -0,0 +1,13 @@
+package com.cydercode.cams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
